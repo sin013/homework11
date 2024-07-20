53008becaa5727951519164b7d5c434bce150d29
sin013 <158771236+sin013@users.noreply.github.com>
2024-07-20 17:40:35 +0300

    Add files via upload

--- homework11.py
+++ homework11.py
@@ -1,20 +1,19 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 169, -15]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 169, -15, 81, 121, 0]
 Primes = []
 notPrimes = []
 for i in numbers:
-    for j in (2, i):
-        if i == j:
+    IsPrime = True
+    if i < 1:
+        IsPrime = False
+    if i == 1:
+        continue
+    for j in range(2, i):
+        if i % j == 0:
+            IsPrime = False
             break
-        elif i == 2:
-            Primes.append(i)
-        elif i % j == 0 and (i != j):
-            notPrimes.append(i)
-        elif i < 1:
-            notPrimes.append(i)
-        elif i == 1:
-            i += 1
-        else:
-            Primes.append(i)
-print(f'простые:', Primes)
-print(f'не простые:', notPrimes)
-
+    if IsPrime == True:
+        Primes.append(i)
+    if IsPrime == False:
+        notPrimes.append(i)
+print('простые:', Primes)
+print('не простые:', notPrimes)
